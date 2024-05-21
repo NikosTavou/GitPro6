3d9dc6d2b8d287d1af99648c556e65ee7c153248
Nikos Tavoulareas <nikolarasone1@gmail.com>
2024-05-21 19:38:38 +0300

    Hello Coding Factory with many bangs

--- src/gr/aueb/cf/hello.java
+++ src/gr/aueb/cf/hello.java
@@ -2,6 +2,7 @@ package gr.aueb.cf;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("Hello Coding Factory!!!!!!!!");
     }
+
 }
